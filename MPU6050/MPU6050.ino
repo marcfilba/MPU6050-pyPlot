aafb4a8a6c3f0ec31da0d5b45d115ac53c640f4b
Marc Filbà <marcfilba@lemming.cat>
2016-02-28 14:33:31 +0100

    Afegit showHelp

--- MPU6050/MPU6050.ino
+++ MPU6050/MPU6050.ino
@@ -35,6 +35,27 @@ int equivGyro = MPU6050_GYRO_FS_250_EQUIV;
 
 bool acquisitionActivated = false;
 
+void showHelp (){
+  Serial.println ("1 - Show help");
+  Serial.println ("2 - Start acquisition");
+  Serial.println ("3 - Stop acquisition");
+
+  Serial.println ("4 - Set AccelRange to +- 2G");
+  Serial.println ("5 - Set AccelRange to +- 4G");
+  Serial.println ("6 - Set AccelRange to +- 8G");
+  Serial.println ("7 - Set AccelRange to +- 16G");
+  
+  Serial.println ("8 - Set GyroRange to +- 250 degrees/sec");
+  Serial.println ("9 - Set GyroRange to +- 500 degrees/sec");
+  Serial.println ("10 - Set GyroRange to +- 1000 degrees/sec");
+  Serial.println ("11 - Set GyroRange to +- 2000 degrees/sec");
+  
+  Serial.println ("12 - Increment acquisition time in 5 ms");
+  Serial.println ("13 - Increment acquisition time in 50 ms");
+  Serial.println ("14 - Decrement acquisition time in 5 ms");
+  Serial.println ("15 - Decrement acquisition time in 50 ms\n");
+}
+
 int getOption (){
   int option = -1;
   while (Serial.available() > 0){
@@ -53,81 +74,85 @@ void handleOption (){
   switch (getOption ()){
     case -1: break;
     case 1:{ 
+      showHelp();
+    break;
+    }
+    case 2:{ 
       acquisitionActivated = true;
       Serial.println("Starting acquisition");  
       break;
      }
-     case 2:{ 
+     case 3:{ 
       acquisitionActivated = false;
       Serial.println("Stoping acquisition");  ;  
       break;
      }
-    case 3:{ 
+    case 4:{ 
       accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
       Serial.println("AccelRange setted to 2G");  
       equivAccel = MPU6050_ACCEL_FS_2_EQUIV; 	           	
       break;
      }
-    case 4:{ 
+    case 5:{ 
       accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_4);
       Serial.println("AccelRange setted to 4G");  
       equivAccel = MPU6050_ACCEL_FS_4_EQUIV; 	           	
       break; 
     }
-    case 5:{ accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+    case 6:{ accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
       Serial.println("AccelRange setted to 8G");  
       equivAccel = MPU6050_ACCEL_FS_8_EQUIV; 		          
       break;
     }
-    case 6:{ 
+    case 7:{ 
       accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
       Serial.println("AccelRange setted to 16G"); 
       equivAccel = MPU6050_ACCEL_FS_16_EQUIV;		          
       break;
     }
-    case 7:{ 
+    case 8:{ 
       accelgyro.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
       Serial.println("GyroRange setted to 250 degrees/sec");	
       equivGyro = MPU6050_GYRO_FS_250_EQUIV;  
       break;
     }
-    case 8:{ 
+    case 9:{ 
       accelgyro.setFullScaleGyroRange(MPU6050_GYRO_FS_500);
       Serial.println("GyroRange setted to 500 degrees/sec");	
       equivGyro = MPU6050_GYRO_FS_500_EQUIV;
       break;
     }
-    case 9:{ 
+    case 10:{ 
       accelgyro.setFullScaleGyroRange(MPU6050_GYRO_FS_1000); 
       Serial.println("GyroRange setted to 1000 degrees/sec");	
       equivGyro = MPU6050_GYRO_FS_1000_EQUIV;
       break;
     }
-    case 10:{ 
+    case 11:{ 
       accelgyro.setFullScaleGyroRange(MPU6050_GYRO_FS_2000); 
       Serial.println("GyroRange setted to 2000 degrees/sec");	
       equivGyro = MPU6050_GYRO_FS_2000_EQUIV; 
       break;
     }
-    case 11:{ 
+    case 12:{ 
       Serial.println ("Incrementing acquisition time in 5 ms");
       acquisitionTime = min (acquisitionTime + 5, 1000);
       Serial.print ("Acquisition time setted to "); Serial.print (acquisitionTime); Serial.println (" ms");
       break;
     }
-    case 12:{ 
+    case 13:{ 
       Serial.println ("Incrementing acquisition time in 50 ms"); 
       acquisitionTime = min (acquisitionTime + 50, 1000);
       Serial.print ("Acquisition time setted to "); Serial.print (acquisitionTime); Serial.println (" ms");
       break;
     }
-    case 13:{ 
+    case 14:{ 
       Serial.println("Decrementing acquisition time in 5 ms");
       acquisitionTime = max (acquisitionTime - 5, 5);
       Serial.print ("Acquisition time setted to "); Serial.print (acquisitionTime); Serial.println (" ms");
       break;
     }
-    case 14:{ 
+    case 15:{ 
       Serial.println("Decrementing acquisition time in 50 ms");  
       acquisitionTime = max (acquisitionTime - 50, 5);
       Serial.print ("Acquisition time setted to "); Serial.print (acquisitionTime); Serial.println (" mz");
@@ -154,23 +179,7 @@ void setup(){
   Serial.println ("Setted AccelRange to +- 2G");
   Serial.println ("Setted GyroRange to +- 250 degrees/sec\n");
 
-  Serial.println ("1 - Start acquisition");
-  Serial.println ("2 - Stop acquisition");
-
-  Serial.println ("3 - Set AccelRange to +- 2G");
-  Serial.println ("4 - Set AccelRange to +- 4G");
-  Serial.println ("5 - Set AccelRange to +- 8G");
-  Serial.println ("6 - Set AccelRange to +- 16G");
-  
-  Serial.println ("7 - Set GyroRange to +- 250 degrees/sec");
-  Serial.println ("8 - Set GyroRange to +- 500 degrees/sec");
-  Serial.println ("9 - Set GyroRange to +- 1000 degrees/sec");
-  Serial.println ("10 - Set GyroRange to +- 2000 degrees/sec");
-  
-  Serial.println ("11 - Increment acquisition time in 5 ms");
-  Serial.println ("12 - Increment acquisition time in 50 ms");
-  Serial.println ("13 - Decrement acquisition time in 5 ms");
-  Serial.println ("14 - Decrement acquisition time in 50 ms\n");
+  showHelp ();
 }
 
 void loop(){
