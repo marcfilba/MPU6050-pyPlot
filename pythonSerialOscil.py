e676e16969cbdfc85464a8fcc58e19a49e9f80bd
Marc Filbà <marcfilba@lemming.cat>
2016-03-01 16:55:05 +0100

    Corregit codi python i afegides les screenShots

--- pythonSerialOscil.py
+++ pythonSerialOscil.py
@@ -1,4 +1,5 @@
 import serial
+import select
 import numpy as np
 
 from sys import stdin
@@ -40,73 +41,84 @@ temp, = plt.plot(tempData)
 
 plt.ylim([0,0])
 
+
+rawData = 'AccelRange'
+rawDataSplitted = []
+
+read = False
+
+print ''
+
 while True:
 
-    rawData = ""
-    rawDataSplitted = []
-    while len (rawDataSplitted) != 7 or not 'AccelRange' in rawData or not 'GyroRange' in rawData:
-        rawData = ser.readline().rstrip()	# llegim accel, gyro i temp
-        rawDataSplitted = rawData.split (",")
+	if select.select([ser,],[],[],0.0)[0]:
+		read = True
+		rawData = ser.readline().rstrip()	# llegim accel, gyro i temp
+		rawDataSplitted = rawData.split (",")
 
-    if sys.stdin.isatty():
-        ser.write(stdin.readline())
-
-    if len(rawDataSplitted) == 7:
-    	xAccelData.append(float (str(rawData[0])))
-    	yAccelData.append(float (str(rawData[1])))
-    	zAccelData.append(float (str(rawData[2])))
-
-    	xAnVelData.append(float (str(rawData[3])))
-    	yAnVelData.append(float (str(rawData[4])))
-    	zAnVelData.append(float (str(rawData[5])))
-
-    	tempData.append (float (str(rawData[6])))
-
-    	plt.subplot(311)
-    	plt.ylim([\
-    				min (min (xAccelData), min (yAccelData), min (zAccelData)) -200, \
-    				max (max (xAccelData), max (yAccelData), max (zAccelData)) + 200])
-
-    	plt.subplot(312)
-    	plt.ylim([\
-    				min (min (xAnVelData), min (yAnVelData), min (zAnVelData)) -200, \
-    				max (max (xAnVelData), max (yAnVelData), max (zAnVelData)) + 200])
-
-    	plt.subplot(313)
-    	plt.ylim([min (tempData)-3,max (tempData)+3])
-
-    	del xAccelData [0]
-    	del yAccelData [0]
-    	del zAccelData [0]
-
-    	del xAnVelData [0]
-    	del yAnVelData [0]
-    	del zAnVelData [0]
-
-    	del tempData [0]
-
-    	xAccel.set_xdata(np.arange(len(xAccelData)))
-    	xAccel.set_ydata(xAccelData)
-
-    	yAccel.set_xdata(np.arange(len(yAccelData)))
-    	yAccel.set_ydata(yAccelData)
-
-    	zAccel.set_xdata(np.arange(len(zAccelData)))
-    	zAccel.set_ydata(zAccelData)
-
-
-    	xAnVel.set_xdata(np.arange(len(xAnVelData)))
-    	xAnVel.set_ydata(xAnVelData)
-
-    	yAnVel.set_xdata(np.arange(len(yAnVelData)))
-    	yAnVel.set_ydata(yAnVelData)
-
-    	zAnVel.set_xdata(np.arange(len(zAnVelData)))
-    	zAnVel.set_ydata(zAnVelData)
-
-    	temp.set_xdata(np.arange(len(tempData)))
-    	temp.set_ydata(tempData)
-
-    	plt.draw()
-    else:
+		#if len (rawDataSplitted) != 7:
 		print rawData
+	else:
+		read = False
+
+	while select.select([stdin,],[],[],0.0)[0]:
+		ser.write(stdin.readline())
+
+	if len(rawDataSplitted) == 7 and read:
+
+		xAccelData.append(float (str(rawDataSplitted[0])))
+		yAccelData.append(float (str(rawDataSplitted[1])))
+		zAccelData.append(float (str(rawDataSplitted[2])))
+
+	 	xAnVelData.append(float (str(rawDataSplitted[3])))
+		yAnVelData.append(float (str(rawDataSplitted[4])))
+		zAnVelData.append(float (str(rawDataSplitted[5])))
+
+		tempData.append (float (str(rawDataSplitted[6])))
+
+		plt.subplot(311)
+		plt.ylim([\
+				min (min (xAccelData), min (yAccelData), min (zAccelData)) - 1, \
+				max (max (xAccelData), max (yAccelData), max (zAccelData)) + 1])
+
+		plt.subplot(312)
+		plt.ylim([\
+				min (min (xAnVelData), min (yAnVelData), min (zAnVelData)) - 1, \
+				max (max (xAnVelData), max (yAnVelData), max (zAnVelData)) + 1])
+
+		plt.subplot(313)
+		plt.ylim([min (tempData)-3,max (tempData)+3])
+
+		del xAccelData [0]
+		del yAccelData [0]
+		del zAccelData [0]
+
+		del xAnVelData [0]
+		del yAnVelData [0]
+		del zAnVelData [0]
+
+		del tempData [0]
+
+		xAccel.set_xdata(np.arange(len(xAccelData)))
+		xAccel.set_ydata(xAccelData)
+
+		yAccel.set_xdata(np.arange(len(yAccelData)))
+		yAccel.set_ydata(yAccelData)
+
+		zAccel.set_xdata(np.arange(len(zAccelData)))
+		zAccel.set_ydata(zAccelData)
+
+
+		xAnVel.set_xdata(np.arange(len(xAnVelData)))
+		xAnVel.set_ydata(xAnVelData)
+
+		yAnVel.set_xdata(np.arange(len(yAnVelData)))
+		yAnVel.set_ydata(yAnVelData)
+
+		zAnVel.set_xdata(np.arange(len(zAnVelData)))
+		zAnVel.set_ydata(zAnVelData)
+
+		temp.set_xdata(np.arange(len(tempData)))
+		temp.set_ydata(tempData)
+
+		plt.draw()
